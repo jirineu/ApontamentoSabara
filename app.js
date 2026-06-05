d24ff16902fc868d9e6585609ad7bf72708b510e
jirineu <moizecape@gmail.com>
2026-06-05 17:21:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -986,7 +986,7 @@ async function atualizarDadosDaPlanilha() {
         botao.disabled = true;
 
         // URL gerada na implantação do seu Google Apps Script
-        const urlWebApp = "https://script.google.com/macros/s/AKfycbw36Zv_IdusCQWqMsqswymxSNQ5NjDULUQ_KebVRonzRTPR7Z6rDTtXqwfRodRc6guMPg/exec"; 
+        const urlWebApp = "https://script.google.com/macros/s/AKfycbxyGYBv-kXH3hsLJQtluGi6fbmgkpV8GYwLfSpJ-0jhbXG47BCzj8ZUZHB4jw_TH9hw/exec"; 
         
         const resposta = await fetch(urlWebApp);
         const dados = await resposta.json();
